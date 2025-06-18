1a5260f3c2676cd595a6e1c9d2a81724b2964bb2
Ahsen <ahsenmaqbool3112114@gmail.com>
2025-06-18 23:05:12 +0500

    uibuild

--- index.js
+++ index.js
@@ -46,14 +46,8 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 // create
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
 
   const note = new Note({
     content: body.content,
@@ -63,6 +57,7 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
 // update
@@ -107,6 +102,10 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: "malformatted id"})
+  }
+
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message})
   }
 
   next(error)
